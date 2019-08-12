b78807511913fdea65a4c5e0ed7afe8184099b89
Abdusaid10 <Abdusaid10@github.com>
2019-08-12 21:03:12 +0500

    added promt

--- lib/search.rb
+++ lib/search.rb
@@ -3,7 +3,6 @@ require 'nokogiri'
 
 
 
-res_google= RestClient.get "https://google.com/search/", {params: {q: "SEO"}}
 def bing(string)
     res =  RestClient.get "https://bing.com/search/", {params: {q: string}}
     tags = Nokogiri::HTML.parse(res).xpath("//a")
@@ -31,15 +30,6 @@ def google(string)
         end
     end
 end
-
+puts "Enter something to search"
 string=gets.chomp
 bing(string)
-# tags.each do |tag|
-#     if !tag[:href].nil? && tag[:href].start_with?("https://") && tag[:href].length < 100
-#         puts "-----------------------------------------"
-#         puts 
-#         puts "#{tag.text}"
-#         puts "#{tag[:href]}"
-#         puts
-#     end
-# end
