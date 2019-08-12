7bc5e539fc8079a4c8d9d09e9954018114d2a007
Abdusaid10 <Abdusaid10@github.com>
2019-08-12 16:22:14 +0500

    added some staff

--- lib/search.rb
+++ lib/search.rb
@@ -3,14 +3,43 @@ require 'nokogiri'
 
 
 
-res =  RestClient.get "https://bing.com/search/", {params: {q: 'hello world'}}
-tags = Nokogiri::HTML.parse(res).xpath("//a")
-tags.each do |tag|
-    if !tag[:href].nil? && tag[:href].start_with?("https://") && tag[:href].length < 100
-        puts "-----------------------------------------"
-        puts 
-        puts "#{tag.text}"
-        puts "#{tag[:href]}"
-        puts
+res_google= RestClient.get "https://google.com/search/", {params: {q: "SEO"}}
+def bing(string)
+    res =  RestClient.get "https://bing.com/search/", {params: {q: string}}
+    tags = Nokogiri::HTML.parse(res).xpath("//a")
+    tags.each do |tag|
+        if !tag[:href].nil? && tag[:href].start_with?("https://") && tag[:href].length < 100
+            puts "-----------------------------------------"
+            puts 
+            puts "#{tag.text}"
+            puts "#{tag[:href]}"
+            puts
+        end
     end
 end
+
+def google(string)
+    res_google= RestClient.get "https://google.com/search/", {params: {q: string}}
+    tags = Nokogiri::HTML.parse(res).xpath("//a")
+    tags.each do |tag|
+        if !tag[:href].nil? && tag[:href].start_with?("https://") && tag[:href].length < 100
+            puts "-----------------------------------------"
+            puts 
+            puts "#{tag.text}"
+            puts "#{tag[:href]}"
+            puts
+        end
+    end
+end
+
+string=gets.chomp
+bing(string)
+# tags.each do |tag|
+#     if !tag[:href].nil? && tag[:href].start_with?("https://") && tag[:href].length < 100
+#         puts "-----------------------------------------"
+#         puts 
+#         puts "#{tag.text}"
+#         puts "#{tag[:href]}"
+#         puts
+#     end
+# end
