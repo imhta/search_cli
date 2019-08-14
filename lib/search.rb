b8abac239f4a7a3bb3af68c1065fc0c67fb8c9c4
imhta <imhtapm@gmail.com>
2019-08-14 22:56:32 +0530

    added before after text

--- lib/search.rb
+++ lib/search.rb
@@ -6,19 +6,23 @@ require_relative './display_module'
 
 class SearchProviders
   def self.bing(keyword)
+    Display.before_result
     res =  RestClient.get 'https://bing.com/search/', params: { q: keyword }
     tags = Nokogiri::HTML.parse(res).xpath('//li[@class="b_algo"] //h2 //a')
     tags.each do |tag|
       Display.tags tag
     end
+    Display.after_result
   end
 
   def self.gems(keyword)
+    Display.before_result
     res = RestClient.get "https://rubygems.org/search?utf8=%E2%9C%93&query=#{keyword}"
     tags = Nokogiri::HTML.parse(res).xpath('//a[@class="gems__gem"]')
     tags.each do |tag|
       Display.tags tag
     end
+    Display.after_result
   end
 
   def self.so(keyword)
